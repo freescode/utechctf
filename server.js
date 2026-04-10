dc39007f008391e7b3fe864fb4875519c16fcb4c
freescode <fostermerrick7@gmail.com>
2026-04-09 19:59:20 -0500

    fix all localhost URLs to relative paths

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
